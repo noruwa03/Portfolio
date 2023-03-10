3806a80149fb9a4ca86877be7eea81f790c58adb
noruwa03 <noruwaobaseki@gmail.com>
2023-03-10 14:34:09 +0100

    Changed social media svg fill colors

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import MyImage from "./assets/images/Obaseki_Noruwa.jpg";
 import Haraka from "./assets/images/haraka.png";
 import Ecolabz from "./assets/images/ecolabz.png";
 import { useRef } from "react";
+
 function App() {
   const menuBtn = useRef<HTMLDivElement | null>(null);
   const navItem = useRef<HTMLDivElement | null>(null);
@@ -172,7 +173,7 @@ function App() {
                 width="18"
                 height="18"
                 fill="currentColor"
-                className="bi bi-moon-stars-fill fill-gray-600"
+                className="bi bi-moon-stars-fill lg:fill-gray-600 fill-white"
                 viewBox="0 0 16 16"
                 ref={moon}
                 onClick={themeSwitch}
@@ -206,7 +207,7 @@ function App() {
                 strokeWidth="2"
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                className="feather feather-github transition ease-in-out motion-reduce:transition-none motion-reduce:hover:transform-none  hover:-translate-y-1 stroke-indigo-800 hover:stroke-red-500"
+                className="feather feather-github transition ease-in-out motion-reduce:transition-none motion-reduce:hover:transform-none  hover:-translate-y-1 stroke-indigo-800 hover:stroke-red-400"
               >
                 <title>GitHub</title>
                 <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
@@ -226,7 +227,7 @@ function App() {
                 strokeWidth="2"
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                className="feather feather-twitter transition ease-in-out motion-reduce:transition-none motion-reduce:hover:transform-none  hover:-translate-y-1 stroke-indigo-800 hover:stroke-red-500"
+                className="feather feather-twitter transition ease-in-out motion-reduce:transition-none motion-reduce:hover:transform-none  hover:-translate-y-1 stroke-indigo-800 hover:stroke-red-400"
               >
                 <title>Twitter</title>
                 <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
@@ -246,7 +247,7 @@ function App() {
                 strokeWidth="2"
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                className="feather feather-linkedin transition ease-in-out motion-reduce:transition-none motion-reduce:hover:transform-none  hover:-translate-y-1 stroke-indigo-800 hover:stroke-red-500"
+                className="feather feather-linkedin transition ease-in-out motion-reduce:transition-none motion-reduce:hover:transform-none  hover:-translate-y-1 stroke-indigo-800 hover:stroke-red-400"
               >
                 <title>LinkedIn</title>
                 <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
